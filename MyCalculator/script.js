950cd4fc0b459a98369e0ee996f05c73269cdc56
naveensharmatezminds <naveen@tezminds.com>
2023-09-28 17:53:10 +0530

    done

--- MyCalculator/script.js
+++ MyCalculator/script.js
@@ -1,16 +1,34 @@
+
+let isCalculated = false;
+
 function clearScreen() {
     document.getElementById("display").value = "";
 }
 
 
 function display(val) {
-    document.getElementById("display").value += val;
-    console.log(val);
+    let button = document.getElementById("display").value
+    if(isCalculated){
+        if(button == "+" || button == "-" || button == "/" || button == "*"){
+            document.getElementById("display").value += val;
+            isCalculated = false;
+        }else{
+            document.getElementById("display").value = "";
+            document.getElementById("display").value += val;
+            isCalculated = false;
+        }
+    }else{
+        document.getElementById("display").value += val;
+    }
+
+    // document.getElementById("display").value += val;
+    // console.log(val);
 }
  
 
 function cal() {
     var p = document.getElementById("display").value;
     var q = eval(p);
+    isCalculated = true;
     document.getElementById("display").value = q;
 }
